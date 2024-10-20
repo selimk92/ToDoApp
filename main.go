c37d81bd368dd08a03953c550e073208995c8d79
selimk92 <selim.kucukgul@gmail.com>
2024-10-21 00:45:00 +0300

    http-todolist-with handle

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"net/http"
 )
 
+var shortGolang = "Watch Go crash course"
+var fullGolang = "Watch Nana's Golang Full course"
+var rewardDessert = "Reward myself with a donut"
+var taskItems = []string{shortGolang, fullGolang, rewardDessert}
+
 func main() {
 
-	http.HandleFunc("/hello-go", helloUser)
+	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/show-tasks", showTasks)
 
 	http.ListenAndServe(":8080", nil)
 
@@ -19,20 +25,11 @@ func helloUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, greeting)
 
 }
-func printTask(taskItems2 []string) {
 
-	fmt.Println("List of my Todos")
+func showTasks(w http.ResponseWriter, r *http.Request) {
+	for _, task := range taskItems {
+		fmt.Fprintln(w, task)
 
-	for index, task := range taskItems2 {
-		index = index + 1
-		fmt.Println(index, ":", task)
 	}
-
-}
-
-func addTask(taskItems1 []string, newTask string) []string {
-
-	var updatedTaskItems = append(taskItems1, newTask)
-	return updatedTaskItems
 
 }
